0458e1e241fc79955e65d5c50cf702346b264d0f
bourgeois hector <hectorbourgeois.56@gmail.com>
2025-09-01 12:06:16 +0200

    modification on model shape, for running basic deppmodels

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -26,6 +26,75 @@ def fit_model(model, X, y) :
 def score_model(model, X, y) :
     return model.score(X,y)
 
+
+
+
+##########################
+######### DEEP ###########
+##########################
+
+def initialize_deep_dense_model(X_train):
+    #reg_l1 = regularizers.L1(0.01)
+    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
+    model = Sequential()
+    model.add(Input(shape = (X_train.shape[1],1) ))
+
+    ### First layer
+    model.add(layers.Dense(300, activation='relu'))
+
+    ### second layer
+    model.add(layers.Dense(200,activation='relu'))
+
+    ### third layer
+    model.add(layers.Dense(126,activation='relu'))
+    model.add(layers.Dropout(rate= 0.2))
+
+    ### output layer
+    model.add(layers.Dense(1, activation='relu'))
+
+    return model
+
+def initialize_deep_rnn_model(X_train):
+    #reg_l1 = regularizers.L1(0.01)
+    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
+    ## RNN
+    rnn = Sequential()
+    rnn.add(Input(shape=(X_train.shape[1],1))),
+    rnn.add(layers.LSTM(50)),
+
+    rnn.add(layers.Dense(1, activation="relu"))
+    return rnn
+
+def initialize_deep_cnn_model(X_train):
+    #reg_l1 = regularizers.L1(0.01)
+    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
+    # Conv1D
+    cnn = Sequential()
+    cnn.add(Input(shape=(X_train.shape[1],1))), #X_train.shape[1:]
+    cnn.add(layers.Conv1D(20, kernel_size=(1))),
+    cnn.add(layers.Dense(1, activation="relu"))
+    return cnn
+
+def compile_deep_model(model):
+    '''return a compiled model suited for the CIFAR-10 task'''
+    model.compile(
+        loss = 'mae',
+        optimizer = 'adam',
+        metrics = ['mae']
+    )
+    return model
+
+def fit_deep_model(model,X_train,y_train,validation_data, batch_size = 32, epochs= 200):
+    es = EarlyStopping(patience = 20, restore_best_weights=True)
+    history = model.fit(X_train,
+          y_train,
+          validation_data= validation_data,
+          batch_size = batch_size,
+          epochs= epochs,
+          callbacks = [es])
+    return history, model
+
+
 if __name__ == "__main__":
 
     from data import load_data, player_full_data_df, y_creator
@@ -69,73 +138,3 @@ if __name__ == "__main__":
     # accuracy = cv_results["test_score"].mean()
 
     print("Test good (✅ pour Flavian)")
-
-
-
-##########################
-######### DEEP ###########
-##########################
-
-def initialize_deep_dense_model(X_train):
-    #reg_l1 = regularizers.L1(0.01)
-    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
-    model = Sequential()
-    model.add(Input(shape = X_train.shape[1:] ))
-
-    ### First layer
-    model.add(layers.Dense(300, activation='relu'))
-
-    ### second layer
-    model.add(layers.Dense(200,activation='relu'))
-
-    ### third layer
-    model.add(layers.Dense(126,activation='relu'))
-    model.add(layers.Dropout(rate= 0.2))
-
-    ### output layer
-    model.add(layers.Dense(1, activation='relu'))
-
-    return model
-
-def initialize_deep_rnn_model(X_train):
-    #reg_l1 = regularizers.L1(0.01)
-    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
-    ## RNN
-    rnn = Sequential(
-        model.add(Input(shape=(423, 1))),
-        model.add(layers.LSTM(50)),
-        model.add(layers.LSTM(100)),
-        model.add(layers.LSTM(20)),
-        model.add(layers.Dense(1, activation="relu"))
-    )
-    return rnn
-
-def initialize_deep_cnn_model(X_train):
-    #reg_l1 = regularizers.L1(0.01)
-    '''instanciate and return the CNN architecture of your choice with less than 150,000 params'''
-    # Conv1D
-    cnn = Sequential(
-        model.add(Input(shape=X_train.shape[1:])),
-        model.add(layers.Conv1D(20, kernel_size=1)),
-        model.add(layers.Dense(1, activation="relu"))
-        )
-    return cnn
-
-def compile_deep_model(model):
-    '''return a compiled model suited for the CIFAR-10 task'''
-    model.compile(
-        loss = 'mae',
-        optimizer = 'adam',
-        metrics = ['mae']
-    )
-    return model
-
-def fit_deep_model(model,X_train,y_train,validation_data, batch_size = 32, epochs= 200):
-    es = EarlyStopping(patience = 20, restore_best_weights=True)
-    history = model.fit(X_train,
-          y_train,
-          validation_data= validation_data,
-          batch_size = batch_size,
-          epochs= epochs,
-          callbacks = [es])
-    return history, model
